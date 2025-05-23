b48769a133965f27fe2ae865aaba1b2e61ef8f8b
yunaan <yunaan@mymusictaste.com>
2025-05-23 22:05:52 +0900

    feat(yuna-invite-wedding): Fix mobile zoom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body>
         <StyledProvider>{children}</StyledProvider>
         <NaverMapLoader />
