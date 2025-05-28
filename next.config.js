52e445944e12ae938f92079077cf9d2d476b844f
yunaan <yunaan@mymusictaste.com>
2025-05-28 18:27:54 +0900

    feat(yuna-invite-wedding): Add CloudFront log analyzer workflow

--- next.config.js
+++ next.config.js
@@ -6,14 +6,9 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true, //개발 중 우선 배포를 위한 임시 설정입니다.
   },
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: "https",
-  //       hostname: "cdn.pixabay.com",
-  //     },
-  //   ],
-  // },
+  images: {
+    domains: ["assets.sco-yuna.kr"],
+  },
 };
 
 module.exports = nextConfig;
